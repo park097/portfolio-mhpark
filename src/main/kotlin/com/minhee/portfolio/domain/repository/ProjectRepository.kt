ecd12770e30d4a5e74aa3f0a03eb885bd24378a5
박민희 <alslalslclq24@naver.com>
2025-06-22 21:58:02 +0900

    데이터 베이스 설정

--- src/main/kotlin/com/minhee/portfolio/domain/repository/ProjectRepository.kt
+++ src/main/kotlin/com/minhee/portfolio/domain/repository/ProjectRepository.kt
@@ -1,8 +1,8 @@
 package com.minhee.portfolio.domain.repository
 
-import com.minhee.portfolio.domain.entity.Achievement
-import org.aspectj.apache.bcel.Repository
+import com.minhee.portfolio.domain.entity.Project
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface ProjectRepository : JpaRepository<ProjectRepository, Repository> {
+
+interface ProjectRepository : JpaRepository<Project, Long> {
 }
